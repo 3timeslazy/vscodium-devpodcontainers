9d03a5fcd7ebab33ce659b7ab3e41a9866fc82c2
herrwusel <malleal_primacord@plxbx.eu>
2024-09-18 15:58:06 +0200

    fix: make it work on windows

--- src/devpod/bin.ts
+++ src/devpod/bin.ts
@@ -1,6 +1,7 @@
 import vscode from 'vscode';
 import path from 'path';
 import fs from 'fs';
+import commandExists from 'command-exists';
 
 export async function installDevpod() {
 	const install = { title: 'Install' };
@@ -29,28 +30,8 @@ export async function installDevpod() {
 	}
 }
 
-export function devpodBinExists(): boolean {
-	const envPath = process.env['PATH'] || '';
-	const paths = envPath?.split(path.delimiter);
-	let s = '';
-	for (const p of paths) {
-		const binpath = path.join(p, 'devpod');
-		if (executableFileExists(binpath)) {
-			return true;
-		}
-	}
-	return false;
-}
-
-function executableFileExists(filePath: string): boolean {
-	let exists = true;
-	try {
-		exists = fs.statSync(filePath).isFile();
-		if (exists) {
-			fs.accessSync(filePath, fs.constants.F_OK | fs.constants.X_OK);
-		}
-	} catch (e) {
-		exists = false;
-	}
-	return exists;
+export function devpodBinExists(): Promise<boolean> {
+	return commandExists('devpod')
+    .then(() => true)
+    .catch(() => false);
 }
