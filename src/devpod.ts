85ea939d765062a1f4f2f7f4c4dd895c04221c67
Vladimir Fetisov <vladimir.fetisov@pm.me>
2024-10-16 21:00:52 +0200

    Add better messages text. Taken from @cl0vrfi3ld fork

--- src/devpod.ts
+++ src/devpod.ts
@@ -4,7 +4,7 @@ export async function installDevpod() {
   const install = { title: "Open installation guide" };
   const explain = { title: "What is devpod?" };
   const answer = await vscode.window.showInformationMessage(
-    '"devpod" executable is not found. Please, install it',
+    'DevPod couldn\'t be found on your machine, would you like to install it now?',
     install,
     explain,
   );
@@ -20,10 +20,9 @@ export async function installDevpod() {
 
     case explain: {
       const msg = `
-			DevPod Containers extension uses DevPod CLI for bootstraping dev containers.
-			DevPod implements devcontainers specification, setups SSH and a does lots more.
-			Without them this extension wouldn't exist.
-			It is free software created by Loft Labs.
+			The DevPod Containers extension uses DevPod for bootstraping devcontainers.
+			DevPod implements the devcontainers specification, automatically configures SSH access, and lots more. Without it this extension wouldn't be possible.
+			It is free software created by Loft Labs and its source code can be found here: https://github.com/loft-sh/devpod
 			`;
       const clicked = await vscode.window.showInformationMessage(msg, { title: "Source code" });
       if (clicked) {
